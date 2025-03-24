609eeb6fbe6eec9f6a16fa66820cc697e605f6cc
cscms03 <choi.m4527@gmail.com>
2025-03-24 15:13:28 +0800

    Bugfix on person showing tags two times

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -80,11 +80,9 @@ public class PersonCard extends UiPart<Region> {
             content.putString(person.getEmployeeId().toString());
             clipboard.setContent(content);
         });
+
         contextMenu.getItems().add(copyItem);
         employeeId.setContextMenu(contextMenu);
-        person.getTags().stream()
-                .sorted(Comparator.comparing(tag -> tag.tagName))
-                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
 
         if (person.isUpcomingWithinDays(3)) {
             cardPane.setStyle("-fx-background-color: rgb(85, 147, 255, 0.5);");
